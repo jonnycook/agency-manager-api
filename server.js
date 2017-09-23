49402dea48be4ba9786488452f145d10634fa3f1
Jonathan Cook <jonathan@stratosphere.digital>
2017-09-22 20:52:12 -0400

    added support for realtime data sync

--- server.js
+++ server.js
@@ -3,16 +3,16 @@
 const Hapi = require('hapi');
 
 var config = require('./config');
-
-// var mongoose = require('mongoose');
-// mongoose.Promise = require('bluebird');
-// mongoose.connect(config.mongoConnection);
-
 var Promise = require("bluebird");
 var MongoDB = require("mongodb");
 Promise.promisifyAll(MongoDB);
 
 var JSON = require('json-stringify-date');
+
+var WebSocket = require('ws');
+var wss = new WebSocket.Server({ port: 8080 });
+
+var sockets = [];
 
 var db;
 MongoDB.MongoClient.connect(config.mongoConnection).then((a) => {
@@ -20,11 +20,120 @@ MongoDB.MongoClient.connect(config.mongoConnection).then((a) => {
 });
 
 
-// var Activity = mongoose.model('Activity', mongoose.Schema({
-//   name: String,
-//   category: String,
-//   done: Boolean,
-// }));
+async function auth(token) {
+  return (await (await db.collection('users').find({authKey:token})).toArray()).length;
+}
+
+
+async function handleClientPush(payload) {
+  var mutation = payload.mutation;
+  var collection = db.collection(payload.collection);
+
+  if (mutation.path) {
+    for (let comp of mutation.path) {
+      if (comp[0] == '&') {
+        var doc = await collection.findOne({ _id: payload._id });
+
+        var obj = doc;
+        for (var i = 0; i < mutation.path.length; ++ i) {
+          let comp = mutation.path[i];
+          if (comp[0] == '&') {
+            var id = comp.substr(1);
+            var index = obj.findIndex((el) => el._id == id);
+            mutation.path[i] = index;
+            obj = obj[index];
+          }
+          else {
+            obj = obj[comp];
+          }
+        }
+        break;
+      }
+    }
+  }
+
+  if (mutation.type == 'set') {
+    await collection.update({ _id: payload._id }, {
+      $set: {
+        [mutation.path.join('.')]: mutation.value
+      }
+    }, { upsert: true });
+    return true;
+  }
+  else if (mutation.type == 'unset') {
+    await collection.update({ _id: payload._id }, {
+      $unset: {
+        [mutation.path.join('.')]: ''
+      }
+    }, { upsert: true });
+    return true;
+  }
+  else if (mutation.type == 'remove') {
+    let deleteKey = Math.random();
+
+    if (mutation.key) {
+      await collection.update({ _id: payload._id }, {
+        $pull: { [mutation.path.join('.')]: { _id: mutation.key } }
+      });
+    }
+    else {
+      await collection.update({ _id: payload._id }, {
+        $set: { [mutation.path.concat(mutation.index).join('.')]: deleteKey }
+      });
+      await collection.update({ _id: payload._id }, {
+        $pull: { [mutation.path.join('.')]: deleteKey }
+      });
+
+    }
+    return true;
+  }
+  else if (mutation.type == 'insert') {
+    var path = mutation.path.slice(0, -1);
+    var index = mutation.path[mutation.path.length - 1];
+    await collection.update({ _id: payload._id }, {
+      $push: {
+        [path.join('.')]: {
+          $each: [ mutation.el ],
+          $position: index
+        }
+      }
+    });
+    return true;
+  }
+  else if (mutation.type == 'create') {
+    await collection.insert(mutation.document);
+    return true;
+  }
+  else if (mutation.type == 'delete') {
+    await collection.update({ _id: payload._id }, { $set: {_deleted: true} });
+    return true;
+  }
+}
+
+
+wss.on('connection', (ws) => {
+  sockets.push(ws);
+  ws.on('message', async (data) => {
+    var message = JSON.parse(data);
+    if (message.type == 'subscribe') {
+    }
+    else if (message.type == 'push') {
+      if (!(await auth(message.authKey))) {
+        ws.close();
+      }
+      handleClientPush(message.payload);
+      for (let ws2 of sockets) {
+        if (ws2 != ws) {
+          ws2.send(data);
+        }
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    sockets.splice(sockets.indexOf(ws), 1);
+  });
+});
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -38,53 +147,19 @@ server.connection({
 
 var prefix = '/v1/';
 
-// function create(model) {
-//   return {
-//     method: 'POST',
-//     path: `${prefix}${model.collection.name}`,
-//     handler: async function(request, reply) {
-//       var obj = new model(request.payload);
-//       obj.save();
-//       reply(obj);
-//     }
-//   }
-// }
-
-// function update(model) {
-//   return {
-//     method: 'PATCH',
-//     path: `${prefix}${model.collection.name}/{id*1}`,
-//     handler: async function(request, reply) {
-//       reply(await model.findByIdAndUpdate(request.params.id, { $set: request.payload }, { new: true }).exec());
-//     }
-//   }
-// }
-
-// function list(model, populate=[]) {
-//   return {
-//     method: 'GET',
-//     path: `${prefix}${model.collection.name}`,
-//     handler: async function (request, reply) {
-//       reply(await model.find().populate(populate));
-//     }
-//   };
-// }
-
-// function remove(model) {
-//   return {
-//     method: 'DELETE',
-//     path: `${prefix}${model.collection.name}/{id*1}`,
-//     handler: async function (request, reply) {
-//       reply(await model.findByIdAndRemove(request.params.id).exec());
-//     }
-//   };
-// }
-
-async function auth(token) {
-  return (await (await db.collection('users').find({authKey:token})).toArray()).length;
-}
 
 server.route([
+  {
+    method: 'GET',
+    path: `${prefix}clients/push`,
+    handler: async function(request, reply) {
+      console.log(request.query);
+      for (var ws of sockets) {
+        ws.send(request.query.message);
+      }
+      reply(true);
+    }
+  },
   {
     method: 'POST',
     path: `${prefix}login`,
@@ -109,55 +184,7 @@ server.route([
     handler: async function(request, reply) {
       if (!(await auth(request.headers.authentication))) return reply(false);
       var payload = JSON.parse(request.payload);
-      var mutation = payload.mutation;
-      var collection = db.collection(payload.collection);
-      if (mutation.type == 'set') {
-        await collection.update({ _id: payload._id }, {
-          $set: {
-            [mutation.path.join('.')]: mutation.value
-          }
-        }, { upsert: true });
-        reply(true);
-      }
-      else if (mutation.type == 'unset') {
-        await collection.update({ _id: payload._id }, {
-          $unset: {
-            [mutation.path.join('.')]: ''
-          }
-        }, { upsert: true });
-        reply(true);
-      }
-      else if (mutation.type == 'remove') {
-        let deleteKey = Math.random();
-        await collection.update({ _id: payload._id }, {
-          $set: { [mutation.path.concat(mutation.index).join('.')]: deleteKey }
-        });
-        await collection.update({ _id: payload._id }, {
-          $pull: { [mutation.path.join('.')]: deleteKey }
-        });
-        reply(true);
-      }
-      else if (mutation.type == 'insert') {
-        var path = mutation.path.slice(0, -1);
-        var index = mutation.path[mutation.path.length - 1];
-        await collection.update({ _id: payload._id }, {
-          $push: {
-            [path.join('.')]: {
-              $each: [ mutation.el ],
-              $position: index
-            }
-          }
-        });
-        reply(true);
-      }
-      else if (mutation.type == 'create') {
-        await collection.insert(mutation.document);
-        reply(true);
-      }
-      else if (mutation.type == 'delete') {
-        await collection.update({ _id: payload._id }, { $set: {_deleted: true} });
-        reply(true);
-      }
+      reply(await handleClientPush(payload));
     }
   },
   {
